467de30cbb0fcbafbb07303d7948cb2cec2804c5
Sandy <sandy.congreve@cmeducations.se>
2022-04-25 12:06:11 +0200

    Added Todo list compoenent & added to app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import TodoList from "./components/TodoList";
 
 function App() {
 	return (
 		<div className="App">
 			<h1>COOP TODO</h1>
+			<TodoList />
 		</div>
 	);
 }
