e3ffe0e70d48c7ca1b8dfebf4eb8e3ab19e90f20
Linda Andersson <linda.29.andersson@gmail.com>
2022-04-25 12:09:48 +0200

    Implementation of form with an input for todos.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import InputForm from "./components/InputForm";
 
 function App() {
 	return (
 		<div className="App">
 			<h1>COOP TODO</h1>
+			<InputForm />
 		</div>
 	);
 }
--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, {useState} from "react";
+import { useRecoilState } from "recoil";
+import { todosState } from "../recoil/todos/atom";
 
 function InputForm() {
-    
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useRecoilState(todosState);
+
+  function changeValue(e) {
+    setTodos(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTodos([...todos, input]);
+    setInput("");
+  }
+
   return (
-    <form>
-        <input />
+    <form onSubmit={handleSubmit}>
+      <input value={input} onChange={({target:{value}}) => setInput(value)} type="text" />
+      <button>Add</button>
     </form>
-  )
+  );
 }
 
-export default InputForm
+export default InputForm;
